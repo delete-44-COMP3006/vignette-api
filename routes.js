4c5d9b180807aa122a4cf15f4035fbf1c2b5e5f3
Anthony <daviesanthonye1234@gmail.com>
2020-11-30 17:19:46 +0000

    Prettified json response

--- routes.js
+++ routes.js
@@ -2,11 +2,9 @@ let db = require("./db/submissions");
 
 async function submissionsIndex(request, response) {
   let submissions = await db.getSubmissions();
-  response.setHeader("content-type", "text/json");
+  response.setHeader("content-type", "application/json");
 
-  response.json({
-    "submissions": submissions
-  });
+  response.end(JSON.stringify(submissions));
 }
 
 module.exports.submissionsIndex = submissionsIndex;
