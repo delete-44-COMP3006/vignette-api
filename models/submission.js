58f49c43f8c54858aa4584a4d48838ed89111ae3
Anthony <daviesanthonye1234@gmail.com>
2020-12-17 15:21:54 +0000

    Added tests for content validators. Added custom word count validatorg

--- models/submission.js
+++ models/submission.js
@@ -4,11 +4,17 @@ const submissionSchema = new mongoose.Schema({
   title: {
     type: String,
     required: [true, "Please name your submission"],
-    maxLength: [100, "Title must be under 100 characters in length"]
+    maxlength: [100, "Title must be under 100 characters in length"]
   },
   content: {
     type: String,
-    required: [true, "Please add some content to your submission"]
+    required: [true, "Please add some content to your submission"],
+    validate: {
+      validator: function(v) {
+        return v.trim().replace(/\s+/gi, " ").split(" ").length < 500
+      },
+      message: "Please keep your submission under the 500 word limit",
+    }
   }
 });
 
