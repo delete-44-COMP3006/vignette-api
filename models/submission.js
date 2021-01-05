9322c83a5560d0c567ebfab9ce29b1a0ec86e18f
Anthony <daviesanthonye1234@gmail.com>
2021-01-05 11:38:27 +0000

    Added createdAt timestamps

--- models/submission.js
+++ models/submission.js
@@ -1,30 +1,33 @@
 const mongoose = require("mongoose");
 
-const submissionSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: [true, "Please name your submission"],
-    maxlength: [100, "Title must be under 100 characters in length"],
-  },
-  summary: {
-    type: String,
-    maxlength: [300, "Summary must be under 300 characters in length"],
-  },
-  content: {
-    type: String,
-    required: [true, "Please add some content to your submission"],
-    validate: {
-      validator: function (v) {
-        return v.trim().replace(/\s+/gi, " ").split(" ").length < 500;
+const submissionSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Please name your submission"],
+      maxlength: [100, "Title must be under 100 characters in length"],
+    },
+    summary: {
+      type: String,
+      maxlength: [300, "Summary must be under 300 characters in length"],
+    },
+    content: {
+      type: String,
+      required: [true, "Please add some content to your submission"],
+      validate: {
+        validator: function (v) {
+          return v.trim().replace(/\s+/gi, " ").split(" ").length < 500;
+        },
+        message: "Please keep your submission under the 500 word limit",
       },
-      message: "Please keep your submission under the 500 word limit",
+    },
+    score: {
+      type: Number,
+      default: 0,
     },
   },
-  score: {
-    type: Number,
-    default: 0,
-  },
-});
+  { timestamps: true }
+);
 
 const Submission = mongoose.model("submission", submissionSchema);
 module.exports = Submission;
